371f717e3d5a97be5ba6e6a30593b71fe0f9d700
Max Weinzierl <maxwellweinzierl@gmail.com>
2023-11-19 00:27:25 -0600

    Update builder.py

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -23,7 +23,7 @@ from llava.model import *
 from llava.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 
 
-def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, load_bf16=False, device_map="auto", device="cuda"):
+def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, load_bf16=False, device_map="auto", device="cuda", lora_path = None):
     kwargs = {"device_map": device_map}
 
     if device != "cuda":
@@ -152,5 +152,10 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
         context_len = model.config.max_sequence_length
     else:
         context_len = 2048
-
+    if lora_path is not None:
+        from peft import PeftModel
+        model = PeftModel.from_pretrained(
+            model,
+            lora_path,
+        )
     return tokenizer, model, image_processor, context_len
